5237619d21b9425f7959a44710cb743ecb1abc3c
jayyang1 <jayyang1@gmail.com>
2023-01-29 15:33:24 +0900

    config 변경

--- myproject.cmd
+++ myproject.cmd
@@ -2,6 +2,6 @@
 rem cd C:\Users\jayyang\PycharmProjects\project2023A
 set FLASK_APP=pybo
 set FLASK_DEBUG=true
-set APP_CONFIG_FILE=c:\projects\myproject\config\development.py
+set APP_CONFIG_FILE=C:\Users\jayyang\PycharmProjects\project2023A\config\development.py
 rem C:\Users\jayyang\PycharmProjects\project2023A\venv\Scripts\activate
 flask run
