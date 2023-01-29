9c7d8bb71165ff377bda0026195d2d7aeab01eb1
jayyang1 <jayyang1@gmail.com>
2023-01-29 16:54:33 +0900

    404 페이지 추가

--- myproject.cmd
+++ myproject.cmd
@@ -5,3 +5,5 @@ set FLASK_DEBUG=true
 set APP_CONFIG_FILE=C:\Users\jayyang\PycharmProjects\project2023A\config\development.py
 rem C:\Users\jayyang\PycharmProjects\project2023A\venv\Scripts\activate
 flask run
+
+rem gunicorn --bind 0:5000 "pybo:create_app()"
--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -16,6 +16,10 @@ db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
 
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_envvar('APP_CONFIG_FILE')
@@ -28,7 +32,7 @@ def create_app():
         migrate.init_app(app, db)
     from . import models
 
-    # 블루프린트
+    # 블루 프린트
     from .views import main_views, question_views, answer_views, auth_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
@@ -41,5 +45,8 @@ def create_app():
 
     # markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
+
+    # 오류 페이지
+    app.register_error_handler(404, page_not_found)
 
     return app
